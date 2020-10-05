f8398eca653b9392f1febedb204dca3baa41edfe
Ort <OrthOdOxPlus@yandex.ru>
2020-10-05 17:21:23 +0300

    in Time add extends abstract, but need to change type `value` (time in long, not double)

--- src/ru/orthlus/si/Time.java
+++ src/ru/orthlus/si/Time.java
@@ -1,45 +1,56 @@
 package ru.orthlus.si;
 
 public class Time {
-    public static class MilliSecond{
+    public static class MilliSecond extends AbstractMeasure {
         private long value;
-        public MilliSecond(long value){
-            this.value = value;
+
+        public MilliSecond(long value) {
+            super(value, Prefix.MILLI);
         }
     }
-    public static class Second{
+
+    public static class Second extends AbstractMeasure {
         private long value;
-        public final int milliSecondsInSecond = 1000;
-        public Second(long value){
-            this.value = value;
+        public final int MILLISECONDS_IN_SECOND = 1000;
+
+        public Second(long value) {
+            super(value);
         }
     }
-    public static class Minute{
+
+    public static class Minute extends AbstractMeasure {
         private long value;
-        public final int secondsInMinute = 60;
-        public Minute(long value){
-            this.value = value;
+        public final int SECONDS_IN_MINUTE = 60;
+
+        public Minute(long value) {
+            super(value);
         }
     }
-    public static class Hour{
+
+    public static class Hour extends AbstractMeasure {
         private long value;
-        public final int minutesInHour = 60;
-        public Hour(long value){
-            this.value = value;
+        public final int MINUTES_IN_HOUR = 60;
+
+        public Hour(long value) {
+            super(value);
         }
     }
-    public static class Day{
+
+    public static class Day extends AbstractMeasure {
         private long value;
-        public final int hoursInDay = 24;
-        public Day(long value){
-            this.value = value;
+        public final int HOURS_IN_DAY = 24;
+
+        public Day(long value) {
+            super(value);
         }
     }
-    public static class Week{
+
+    public static class Week extends AbstractMeasure {
         private long value;
-        public final int daysInWeek = 7;
-        public Week(long value){
-            this.value = value;
+        public final int DAYS_IN_WEEK = 7;
+
+        public Week(long value) {
+            super(value);
         }
     }
 }
