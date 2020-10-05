6f1b350858d069c8242cc30a063fcda56b28578c
Ort <OrthOdOxPlus@yandex.ru>
2020-10-05 11:54:08 +0300

    add Prefix

--- src/ru/orthlus/si/Mass.java
+++ src/ru/orthlus/si/Mass.java
@@ -5,54 +5,97 @@ import java.util.ArrayList;
 /**
  * Library for transformation operations in International System of Units
  */
-public class Mass {
-
-    public static class Kilogram{
+public abstract class Mass {
+    public static class Kilogram {
         private double value;
+        private Prefix prefix;
 
-        public double getValue() { return value; }
+        public double getValue() {
+            return value;
+        }
 
-        public Kilogram(double value){
+        public Kilogram(double value) {
             this.value = value;
-            System.out.println();
+            this.prefix = Prefix.KILO;
+        }
+        public Kilogram(double value, Prefix prefix) {
+            this.value = value;
+            this.prefix = prefix;
         }
     }
-    public static class Gram{
+
+    public static class Gram {
         private double value;
+        private Prefix prefix;
 
-        public double getValue() { return value; }
+        public double getValue() {
+            return value;
+        }
 
-        public Gram(double value){
+        public Gram(double value) {
             this.value = value;
+            this.prefix = Prefix.NonePrefix;
+        }
+        public Gram(double value, Prefix prefix) {
+            this.value = value;
+            this.prefix = prefix;
         }
     }
-    public static class MilliGram{
+
+    public static class MilliGram {
         private double value;
+        private Prefix prefix;
 
-        public double getValue() { return value; }
+        public double getValue() {
+            return value;
+        }
 
-        public MilliGram(double value){
+        public MilliGram(double value) {
             this.value = value;
+            this.prefix = Prefix.NonePrefix;
+        }
+        public MilliGram(double value, Prefix prefix) {
+            this.value = value;
+            this.prefix = prefix;
         }
     }
-    public static class Tonne{
+
+    public static class Tonne {
         private double value;
+        private Prefix prefix;
 
-        public double getValue() { return value; }
+        public double getValue() {
+            return value;
+        }
 
-        public Tonne(double value){
+        public Tonne(double value) {
             this.value = value;
+            this.prefix = Prefix.NonePrefix;
+        }
+        public Tonne(double value, Prefix prefix) {
+            this.value = value;
+            this.prefix = prefix;
         }
     }
-    public static class Pound{
+
+    public static class Pound {
         private double value;
+        private Prefix prefix;
 
-        public double getValue() { return value; }
-
-        public Pound(double value){
-            this.value = value;
+        public double getValue() {
+            return value;
         }
-        public Kilogram toKilogram(){
+
+        public Pound(double value) {
+            this.value = value;
+            this.prefix = Prefix.NonePrefix;
+        }
+        public Pound(double value, Prefix prefix) {
+            this.value = value;
+            this.prefix = prefix;
+        }
+
+        public Kilogram toKilogram() {
             return new Kilogram(this.value * 0.4535923745);
         }
     }
--- src/ru/orthlus/si/Prefix.java
+++ src/ru/orthlus/si/Prefix.java
@@ -1,7 +1,108 @@
 package ru.orthlus.si;
 
-public interface Prefix {
-    Prefix toNonePrefix();
-
-    Prefix toMilli();
+public interface Prefix{
+    /**
+     * 10 ^ 0
+     */
+    Prefix NonePrefix = null;
+    /**
+     * 10 ^ 1
+     */
+    Prefix DECA = null;
+    String DECA_MIN = "da";
+    /**
+     * 10 ^ 2
+     */
+    Prefix HECTO = null;
+    String HECTO_MIN = "h";
+    /**
+     * 10 ^ 3
+     */
+    Prefix KILO = null;
+    String KILO_MIN = "k";
+    /**
+     * 10 ^ 6
+     */
+    Prefix MEGA = null;
+    String MEGA_MIN = "M";
+    /**
+     * 10 ^ 9
+     */
+    Prefix GIGA = null;
+    String GIGA_MIN = "G";
+    /**
+     * 10 ^ 12
+     */
+    Prefix TERA = null;
+    String TERA_MIN = "T";
+    /**
+     * 10 ^ 15
+     */
+    Prefix PETA = null;
+    String PETA_MIN = "P";
+    /**
+     * 10 ^ 18
+     */
+    Prefix EXA = null;
+    String EXA_MIN = "E";
+    /**
+     * 10 ^ 21
+     */
+    Prefix ZETTA = null;
+    String ZETTA_MIN = "Z";
+    /**
+     * 10 ^ 24
+     */
+    Prefix YOTTA = null;
+    String YOTTA_MIN = "Y";
+    /**
+     * 10 ^ −1
+     */
+    Prefix DECI = null;
+    String DECI_MIN = "d";
+    /**
+     * 10 ^ −2
+     */
+    Prefix CENTI = null;
+    String CENTI_MIN = "c";
+    /**
+     * 10 ^ −3
+     */
+    Prefix MILLI = null;
+    String MILLI_MIN = "m";
+    /**
+     * 10 ^ −6
+     */
+    Prefix MICRO = null;
+    String MICRO_MIN = "µ";
+    /**
+     * 10 ^ −9
+     */
+    Prefix NANO = null;
+    String NANO_MIN = "n";
+    /**
+     * 10 ^ −12
+     */
+    Prefix PICO = null;
+    String PICO_MIN = "p";
+    /**
+     * 10 ^ −15
+     */
+    Prefix FEMTO = null;
+    String FEMTO_MIN = "f";
+    /**
+     * 10 ^ −18
+     */
+    Prefix ATTO = null;
+    String ATTO_MIN = "a";
+    /**
+     * 10 ^ −21
+     */
+    Prefix ZEPTO = null;
+    String ZEPTO_MIN = "z";
+    /**
+     * 10 ^ −24
+     */
+    Prefix YOCTO = null;
+    String YOCTO_MIN = "y";
 }
