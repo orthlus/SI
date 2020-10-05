dc7be447e8b1e6749ecf06b8f06436ca8383e1cd
Ort <OrthOdOxPlus@yandex.ru>
2020-10-05 16:19:41 +0300

    blank for extensibility

---
+++ src/ru/orthlus/si/MassType.java
@@ -0,0 +1,9 @@
+package ru.orthlus.si;
+
+public interface MassType {
+    String KILOGRAM = "Kilogram";
+    String GRAM = "Gram";
+    String MILLIGRAM = "MilliGram";
+    String TONNE = "Tonne";
+    String POUND = "Pound";
+}
